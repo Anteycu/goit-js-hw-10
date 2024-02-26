0868e0af38f43cf7ae34b4f239665a438d60df80
Anteycu <63295042+Anteycu@users.noreply.github.com>
2024-02-26 13:53:46 +0000

    Deploying to gh-pages from @ Anteycu/goit-js-hw-10@fdb65dbb37313383ed783a0b2d9ccf5c5a0d0418 🚀

--- src/update-coinci-markup.js
+++
@@ -1,9 +0,0 @@
-const countriesContainerRef = document.querySelector('.countries');
-import coincidenseTpl from './templates/coincidense.hbs';
-
-function updateCoincidenseMarkup(coincidense) {
-  const markup = coincidenseTpl(coincidense);
-  countriesContainerRef.insertAdjacentHTML('beforeend', markup);
-}
-
-export default updateCoincidenseMarkup;
--- src/update-countries-markup.js
+++
@@ -1,9 +0,0 @@
-const countriesContainerRef = document.querySelector('.countries');
-import countryTpl from './templates/countries.hbs';
-
-function updateCountryMarkup(countries) {
-  const markup = countryTpl(countries);
-  countriesContainerRef.insertAdjacentHTML('beforeend', markup);
-}
-
-export default updateCountryMarkup;
